8a304637b82ce55fec2765e2f290f424cec751fc
MicaelMsilva <130592248+MicaelMsilva@users.noreply.github.com>
2023-11-22 16:31:57 -0300

    Update and rename codigo ofc to loteria.php

--- loteria.php
+++ loteria.php
@@ -4,7 +4,8 @@ function abertura()
 {
     echo "Olá, somos a rede central administradora de jogos!!!\n";
     $verifica = strtolower(trim(readline("Deseja jogar?\n")));
-    if ($verifica == "sim") {
+    if ($verifica == "sim") 
+    {
         escolher_jogo();
     }
 }
@@ -15,35 +16,36 @@ function escolher_jogo()
     echo "2- Quina.\n";
     echo "3- Lotomania.\n";
     echo "4- Lotofácil.\n";
-
+    
     $opcao = intval(readline("Escolha: "));
-
-    switch ($opcao) {
+    
+    switch ($opcao) 
+    {
         case 1:
-            echo "Mega-Sena\n";
-            echo "Intervalo: 6 a 20 dezenas, números de 1 a 60\n";
-            jogar("Mega-Sena", 6, 20, 60);
-            break;
+        echo "Mega-Sena\n";
+        echo "Intervalo: 6 a 20 dezenas, números de 1 a 60\n";
+        jogar("Mega-Sena", 6, 20, 60);
+        break;
         case 2:
-            echo "Quina\n";
-            echo "Intervalo: 5 a 15 dezenas, números de 1 a 80\n";
-            jogar("Quina", 5, 15, 80);
-            break;
+        echo "Quina\n";
+        echo "Intervalo: 5 a 15 dezenas, números de 1 a 80\n";
+        jogar("Quina", 5, 15, 80);
+        break;
         case 3:
-            echo "Lotomania\n";
-            echo "Intervalo: 50 dezenas fixas, números de 1 a 100\n";
-            jogar("Lotomania", 50, 50, 100);
-            break;
+        echo "Lotomania\n";
+        echo "Intervalo: 50 dezenas fixas, números de 1 a 100\n";
+        jogar("Lotomania", 50, 50, 100);
+        break;
         case 4:
-            echo "Lotofácil\n";
-            echo "Intervalo: 15 a 18 dezenas, números de 1 a 25\n";
-            jogar("Lotofácil", 15, 18, 25);
-            break;
+        echo "Lotofácil\n";
+        echo "Intervalo: 15 a 18 dezenas, números de 1 a 25\n";
+        jogar("Lotofácil", 15, 18, 25);
+        break;
         default:
-            clear();
-            echo "Jogo inválido\n";
-            escolher_jogo();
-            break;
+        clear();
+        echo "Jogo inválido\n";
+        escolher_jogo();
+        break;
     }
 }
 
@@ -51,27 +53,31 @@ function jogar($nome_jogo, $min_dezenas, $max_dezenas, $max_numero)
 {
     $num_apostas = intval(readline("Quantas apostas você deseja gerar? "));
     $num_dezenas = intval(readline("Quantas dezenas por aposta? "));
-
+    
     $jogoVencedor = gerarJogoVencedor($min_dezenas, $max_dezenas, $max_numero);
-
+    
     obterIntervalo($nome_jogo, $min_dezenas, $max_dezenas);
-
+    
     $apostas = gerarAposta($min_dezenas, $max_dezenas, $num_apostas, $num_dezenas, $max_numero, $nome_jogo, $jogoVencedor);
-
+    
     echo "\nApostas geradas para $nome_jogo:\n";
-    foreach ($apostas as $i => $aposta) {
+    foreach ($apostas as $i => $aposta) 
+    {
         echo "Aposta " . ($i + 1) . ": " . implode(", ", $aposta) . " - Acertos: " . contarAcertos($aposta, $jogoVencedor) . "\n";
     }
-
+    
     $mediaAcertos = calcularMediaAcertos($apostas, $jogoVencedor);
     $custoTotal = calcularCusto($min_dezenas, $num_apostas);
-
+    
     echo "\nTotal gasto em reais para $nome_jogo: R$" . number_format($custoTotal, 2) . "\n";
     echo "Média de acertos por jogo: $mediaAcertos\n";
-
-    if (jogarNovamente()) {
+    
+    if (jogarNovamente()) 
+    {
         escolher_jogo();
-    } else {
+    } 
+    else 
+    {
         echo "Obrigado por jogar! Até a próxima.\n";
     }
 }
@@ -86,14 +92,16 @@ function jogarNovamente()
 function gerarJogoVencedor($minDezenas, $maxDezenas, $maxNumero)
 {
     $jogoVencedor = array();
-
-    while (count($jogoVencedor) < $minDezenas) {
+    
+    while (count($jogoVencedor) < $minDezenas) 
+    {
         $novaDezena = rand(1, $maxNumero);
-        if (!in_array($novaDezena, $jogoVencedor)) {
+            if (!in_array($novaDezena, $jogoVencedor)) 
+        {
             $jogoVencedor[] = $novaDezena;
         }
     }
-
+    
     sort($jogoVencedor);
     return $jogoVencedor;
 }
@@ -106,10 +114,11 @@ function contarAcertos($aposta, $jogoVencedor)
 function calcularMediaAcertos($apostas, $jogoVencedor)
 {
     $totalAcertos = 0;
-    foreach ($apostas as $aposta) {
+    foreach ($apostas as $aposta) 
+    {
         $totalAcertos += contarAcertos($aposta, $jogoVencedor);
     }
-
+    
     $numApostas = count($apostas);
     return $numApostas > 0 ? round($totalAcertos / $numApostas, 2) : 0;
 }
@@ -127,22 +136,25 @@ function obterIntervalo($nome_jogo, $min_dezenas, $max_dezenas)
 function gerarAposta($minDezenas, $maxDezenas, $numApostas, $numDezenas, $maxNumero, $nomeJogo, $jogoVencedor)
 {
     $apostas = array();
-    for ($i = 0; $i < $numApostas; $i++) {
+    for ($i = 0; $i < $numApostas; $i++) 
+    {
         $dezenasValidas = range(1, $maxNumero);
         $aposta = array();
-
-        while (count($aposta) < $numDezenas) {
+        
+        while (count($aposta) < $numDezenas) 
+        {
             shuffle($dezenasValidas);
             $novaDezena = array_shift($dezenasValidas);
-            if (!in_array($novaDezena, $aposta)) {
+            if (!in_array($novaDezena, $aposta)) 
+            {
                 $aposta[] = $novaDezena;
             }
         }
-
+        
         sort($aposta);
         $apostas[] = $aposta;
     }
-
+    
     return $apostas;
 }
 
